628da2949e929d0e2f43ede70594a78bf8f16b0b
Gopa11313 <gopalaa9819525993@gmail.com>
2021-07-02 11:42:11 +0545

    seller successfully register

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const path = require("path");
-const cors = require("cors");
+// const cors = require("cors");
 const SellerRoutes = require("./Routes/SellerRoutes");
 // const Bookmark=require("./Routes/Bookmark_router")
 // const Comment=require("./Routes/Comment_router")
-const mongooes = require("./Databse/db");
+const mongooes = require("./Db/Db");
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(SellerRoutes);
 
 app.listen(90);
